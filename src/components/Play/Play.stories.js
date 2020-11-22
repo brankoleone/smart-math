dad55211b040f744cf97e03bb92657a0bd90718c
Branimir Simeonov <branimir.simeonov@endava.com>
2020-11-22 18:38:40 +0200

    Added mixins for responsive breakpoints

--- src/components/Play/Play.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Play from './Play';
-
-storiesOf('Play', module).add('default', () => <Play />);
