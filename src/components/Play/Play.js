aeeb9026f64bdad1a2d9cdcf3025585abebc9982
Branimir Simeonov <branimir.simeonov@endava.com>
2020-11-22 10:21:07 +0200

    Added exercise mock

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Play.scss';
 
-const Play = () => <div className="Play">Play Component</div>;
+const Play = () => (
+	<div className="Play">
+		<div className="exercise">
+			<div className="first-digit">7</div>
+			<div className="sign">x</div>
+			<div className="second-digit">3</div>
+		</div>
+	</div>
+);
 
 Play.propTypes = {};
 
