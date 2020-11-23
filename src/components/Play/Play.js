34192d903f2b17d07734f8397901362fe94ba775
Branko Leone <brankoleone@gmail.com>
2020-11-23 13:15:17 +0200

    Added random digit on refresh

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -4,13 +4,17 @@ import './Play.scss';
 const Play = () => (
 	<div className="Play">
 		<div className="exercise">
-			<div className="first-digit">7</div>
+			<div className="first-digit">{getRandomDigit()}</div>
 			<div className="sign">x</div>
-			<div className="second-digit">3</div>
+			<div className="second-digit">{getRandomDigit()}</div>
 		</div>
 		<div className="equals-sign"></div>
 	</div>
 );
+
+function getRandomDigit() {
+	return Math.floor(Math.random() * 10);
+}
 
 Play.propTypes = {};
 
